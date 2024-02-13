cb9eb508c1ebd2265a75af0151b29a0aae83be70
vidyapannangi <156234249+vidyapannangi@users.noreply.github.com>
2024-02-13 13:55:46 -0500

    insertOne.js

---
+++ insertOne.js
@@ -0,0 +1,12 @@
+db.books.insertOne({
+  title: "Deep Dive into React Hooks",
+  ISBN: "000000000",
+  thumbnailUrl: "",
+  publicationDate: ISODate("2019-01-01T00:00:00.000Z"),
+  authors: ["Ada Lovelace"],
+});
+//Output:
+{
+  acknowledged: true,
+  insertedId: ObjectId('65c3ae164d257fbeae952e2d')
+}//

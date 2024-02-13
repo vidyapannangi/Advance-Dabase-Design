8d0d1c6466296b2ece830b3c50e30901af1a00ff
vidyapannangi <156234249+vidyapannangi@users.noreply.github.com>
2024-02-13 13:57:25 -0500

    insertOne.js

--- insertOne.js
+++ insertOne.js
@@ -5,8 +5,3 @@ db.books.insertOne({
   publicationDate: ISODate("2019-01-01T00:00:00.000Z"),
   authors: ["Ada Lovelace"],
 });
-/Output:
-{
-  acknowledged: true,
-  insertedId: ObjectId('65c3ae164d257fbeae952e2d')
-}/

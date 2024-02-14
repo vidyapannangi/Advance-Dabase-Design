aa092d78dd3e1c169443eff98cb8dae8eee25adb
vidyapannangi <156234249+vidyapannangi@users.noreply.github.com>
2024-02-14 12:53:47 -0500

    section6.py

--- section6.py
+++ section6.py
@@ -1,7 +1,11 @@
-from pymongo import MongoClient
-
-# Connect to MongoDB
-client = MongoClient('mongodb+srv://vidyapannangi:test123@cluster0.6woaqse.mongodb.net/?retryWrites=true&w=majority')
-
-# Access a specific database
-db = client['mydatabase']
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://vidyapannangi:test123@cluster0.6woaqse.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)

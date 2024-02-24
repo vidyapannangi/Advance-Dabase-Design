4fd296d21b99e9e9ddbda597aa15d6a70f718600
kowshikavula <158231420+kowshikavula@users.noreply.github.com>
2024-02-24 13:07:00 -0500

    findmany.py

--- CRUDMID/findmany.py
+++ CRUDMID/findmany.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 import pprint
 
 # Define the MongoDB connection URI
-uri = "mongodb+srv://kowshikavula14:Smileysmiley183@cluster0.prsfy7f.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://vidyapannangi:test123@cluster0.6woaqse.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -19,7 +19,7 @@ try:
     products_collection = db.products
 
     # Define the filter to find products with price greater than 10
-    filter_query = {"price": {"$gt": 10}}
+    filter_query = {"price": {"$gt": 1.5}}
 
     # Find products that match the filter
     cursor = products_collection.find(filter_query)

555ae0138350f42fcc07229a7108f970472cef6f
kowshikavula <158231420+kowshikavula@users.noreply.github.com>
2024-02-24 13:06:02 -0500

    findone.py

--- CRUDMID/findone.py
+++ CRUDMID/findone.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 import pprint
 
 # Define the MongoDB connection URI
-uri = "mongodb+srv://kowshikavula14:Smileysmiley183@cluster0.prsfy7f.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://vidyapannangi:test123@cluster0.6woaqse.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
@@ -20,7 +20,7 @@ try:
 
     # Define the filter to find a specific product
     filter_query = {
-        "_id": ObjectId("65d6404928558977256a9d4a")
+        "_id": ObjectId("65da2a5b44169cd034887097")
     }
 
     # Find one product that matches the filter
@@ -33,5 +33,7 @@ except Exception as e:
     # Print any exceptions that occur
     print(e)
 finally:
+    # Close the MongoClient to release resources
+    client.close()
     # Close the MongoClient to release resources
     client.close()
